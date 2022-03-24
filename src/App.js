90b2ca9714c6867a750aa8a78761fdd078334d25
nschoeb <81247287+nschoeb@users.noreply.github.com>
2022-03-24 17:30:16 -0400

    game skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-
+import Game from "./index.js";
 function App() {
   return (
-   
-  );
+   <Game />
+  )
 }
 
 export default App;
