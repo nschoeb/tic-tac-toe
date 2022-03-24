4f0a16ddd2f9428fdd796508e53cc26212a6ceaf
nschoeb <81247287+nschoeb@users.noreply.github.com>
2022-03-24 18:01:42 -0400

    rendered the game containers with inline CSS just to test

--- src/index.js
+++ src/index.js
@@ -3,10 +3,38 @@ import ReactDOM from "react-dom";
 
 import './index.css';
 
+const Square = () => {
+  return (
+    <div style={{
+      backgroundColor:'gold',
+      margin:10,
+      padding:20,
+    }}>
+      Square
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor:'turquoise',
+      margin:10,
+      padding:20,
+    }}>
+      <Square />
+    </div>
+  )
+}
+
 const Game = () => {
     return (
-        <div>
-            Game
+        <div style={{
+          backgroundColor:'bisque',
+          margin:10,
+          padding:20,  
+        }}>
+            <Board />
         </div>
     )
 };
